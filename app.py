1d9020bc3e25cf0c17a0b39cb29d1ff7467e3339
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2026-01-24 07:23:11 +0545

    fixed security issue by making seperate endpoints

--- app.py
+++ app.py
@@ -8,3 +8,31 @@ app.secret_key = "HELLO123"
 def financial_policy():
     return render_template("index.html")
 
+@app.route ('/automation/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route ('/automation/ClubsEventsFund.html')
+def club():
+    return render_template('ClubsEventsFund.html')
+
+@app.route ('/automation/EntryFeeFund.html')
+def fee():
+    return render_template('EntryFeeFund.html')
+
+@app.route ('/automation/ExibitionFund.html')
+def event():
+    return render_template('ExibitionFund.html')
+
+@app.route ('/automation/FSUInternalFund.html')
+def internal():
+    return render_template('FSUInternalFund.html')
+
+@app.route ('/automation/ResearchAndInnovationFund.html')
+def research():
+    return render_template('ResearchAndInnovationFund.html')
+
+@app.route ('/automation/TravelSubsidy.html')
+def travel():
+    return render_template('TravelSubsidy.html')
+
